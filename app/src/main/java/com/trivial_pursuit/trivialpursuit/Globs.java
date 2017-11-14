ec9b2b6766ed6a9c79ca3f6ddc42f6739da6391c
Grant Guglielmo <grantguglielmo>
2017-11-14 01:42:10 -0600

    Added color selection to local play

--- app/src/main/java/com/trivial_pursuit/trivialpursuit/Globs.java
+++ app/src/main/java/com/trivial_pursuit/trivialpursuit/Globs.java
@@ -5,6 +5,17 @@ package com.trivial_pursuit.trivialpursuit;
  */
 
 public class Globs {
+    public enum Colors{
+        BLUE, PINK, YELLOW, PURPLE, GREEN, ORANGE
+    }
+
     static String qsetPath;
     static int playerCnt;
+
+    static Colors p1;
+    static Colors p2;
+    static Colors p3;
+    static Colors p4;
+    static Colors p5;
+    static Colors p6;
 }
--- app/src/main/java/com/trivial_pursuit/trivialpursuit/LocalPlayColorSel.java
+++ app/src/main/java/com/trivial_pursuit/trivialpursuit/LocalPlayColorSel.java
@@ -1,19 +1,272 @@
 package com.trivial_pursuit.trivialpursuit;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import static com.trivial_pursuit.trivialpursuit.Globs.Colors.BLUE;
+import static com.trivial_pursuit.trivialpursuit.Globs.Colors.GREEN;
+import static com.trivial_pursuit.trivialpursuit.Globs.Colors.ORANGE;
+import static com.trivial_pursuit.trivialpursuit.Globs.Colors.PINK;
+import static com.trivial_pursuit.trivialpursuit.Globs.Colors.PURPLE;
+import static com.trivial_pursuit.trivialpursuit.Globs.Colors.YELLOW;
 
 public class LocalPlayColorSel extends AppCompatActivity {
+    int player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_play_colorsel);
+        Globs.p1 = null;
+        Globs.p2 = null;
+        Globs.p3 = null;
+        Globs.p4 = null;
+        Globs.p5 = null;
+        Globs.p6 = null;
+        player = 1;
+        TextView tv1 = (TextView)findViewById(R.id.textColorSel);
+        tv1.setText("Player " + player + " Select\n   Your Color");
+    }
 
+    public static Drawable convertDrawableToGrayScale(Drawable drawable) {
+        if (drawable == null)
+            return null;
+
+        Drawable res = drawable.mutate();
+        res.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
+        return res;
+    }
+
+    public void blueSel(View v) {
+        switch(player){
+            case 1:
+                Globs.p1 = BLUE;
+                break;
+            case 2:
+                Globs.p2 = BLUE;
+                break;
+            case 3:
+                Globs.p3 = BLUE;
+                break;
+            case 4:
+                Globs.p4 = BLUE;
+                break;
+            case 5:
+                Globs.p5 = BLUE;
+                break;
+            case 6:
+                Globs.p6 = BLUE;
+                break;
+        }
+        if(player == 6 || player == Globs.playerCnt){
+            Intent intent = new Intent(this, LocalPlayGame.class);
+            startActivity(intent);
+        }
+        else {
+            player++;
+            TextView tv1 = (TextView) findViewById(R.id.textColorSel);
+            tv1.setText("Player " + player + " Select\n   Your Color");
+            ImageButton myBtn = (ImageButton)v.findViewById(R.id.imageButton4);
+            myBtn.setEnabled(false);
+            Drawable originalIcon = ContextCompat.getDrawable(getApplicationContext(), R.drawable.bluewheel);
+            Drawable icon = convertDrawableToGrayScale(originalIcon);
+            myBtn.setImageDrawable(icon);
+        }
+    }
+
+    public void pinkSel(View v) {
+        switch(player){
+            case 1:
+                Globs.p1 = PINK;
+                break;
+            case 2:
+                Globs.p2 = PINK;
+                break;
+            case 3:
+                Globs.p3 = PINK;
+                break;
+            case 4:
+                Globs.p4 = PINK;
+                break;
+            case 5:
+                Globs.p5 = PINK;
+                break;
+            case 6:
+                Globs.p6 = PINK;
+                break;
+        }
+        if(player == 6 || player == Globs.playerCnt){
+            Intent intent = new Intent(this, LocalPlayGame.class);
+            startActivity(intent);
+        }
+        else {
+            player++;
+            TextView tv1 = (TextView) findViewById(R.id.textColorSel);
+            tv1.setText("Player " + player + " Select\n   Your Color");
+            ImageButton myBtn = (ImageButton)v.findViewById(R.id.imageButton2);
+            myBtn.setEnabled(false);
+            Drawable originalIcon = ContextCompat.getDrawable(getApplicationContext(), R.drawable.pinkwheel);
+            Drawable icon = convertDrawableToGrayScale(originalIcon);
+            myBtn.setImageDrawable(icon);
+        }
+    }
+
+    public void yellowSel(View v) {
+        switch(player){
+            case 1:
+                Globs.p1 = YELLOW;
+                break;
+            case 2:
+                Globs.p2 = YELLOW;
+                break;
+            case 3:
+                Globs.p3 = YELLOW;
+                break;
+            case 4:
+                Globs.p4 = YELLOW;
+                break;
+            case 5:
+                Globs.p5 = YELLOW;
+                break;
+            case 6:
+                Globs.p6 = YELLOW;
+                break;
+        }
+        if(player == 6 || player == Globs.playerCnt){
+            Intent intent = new Intent(this, LocalPlayGame.class);
+            startActivity(intent);
+        }
+        else {
+            player++;
+            TextView tv1 = (TextView) findViewById(R.id.textColorSel);
+            tv1.setText("Player " + player + " Select\n   Your Color");
+            ImageButton myBtn = (ImageButton)v.findViewById(R.id.imageButton3);
+            myBtn.setEnabled(false);
+            Drawable originalIcon = ContextCompat.getDrawable(getApplicationContext(), R.drawable.yellowwheel);
+            Drawable icon = convertDrawableToGrayScale(originalIcon);
+            myBtn.setImageDrawable(icon);
+        }
+    }
+
+    public void purpleSel(View v) {
+        switch(player){
+            case 1:
+                Globs.p1 = PURPLE;
+                break;
+            case 2:
+                Globs.p2 = PURPLE;
+                break;
+            case 3:
+                Globs.p3 = PURPLE;
+                break;
+            case 4:
+                Globs.p4 = PURPLE;
+                break;
+            case 5:
+                Globs.p5 = PURPLE;
+                break;
+            case 6:
+                Globs.p6 = PURPLE;
+                break;
+        }
+        if(player == 6 || player == Globs.playerCnt){
+            Intent intent = new Intent(this, LocalPlayGame.class);
+            startActivity(intent);
+        }
+        else {
+            player++;
+            TextView tv1 = (TextView) findViewById(R.id.textColorSel);
+            tv1.setText("Player " + player + " Select\n   Your Color");
+            ImageButton myBtn = (ImageButton)v.findViewById(R.id.imageButton5);
+            myBtn.setEnabled(false);
+            Drawable originalIcon = ContextCompat.getDrawable(getApplicationContext(), R.drawable.purplewheel);
+            Drawable icon = convertDrawableToGrayScale(originalIcon);
+            myBtn.setImageDrawable(icon);
+        }
+    }
+
+    public void greenSel(View v) {
+        switch(player){
+            case 1:
+                Globs.p1 = GREEN;
+                break;
+            case 2:
+                Globs.p2 = GREEN;
+                break;
+            case 3:
+                Globs.p3 = GREEN;
+                break;
+            case 4:
+                Globs.p4 = GREEN;
+                break;
+            case 5:
+                Globs.p5 = GREEN;
+                break;
+            case 6:
+                Globs.p6 = GREEN;
+                break;
+        }
+        if(player == 6 || player == Globs.playerCnt){
+            Intent intent = new Intent(this, LocalPlayGame.class);
+            startActivity(intent);
+        }
+        else {
+            player++;
+            TextView tv1 = (TextView) findViewById(R.id.textColorSel);
+            tv1.setText("Player " + player + " Select\n   Your Color");
+            ImageButton myBtn = (ImageButton)v.findViewById(R.id.imageButton6);
+            myBtn.setEnabled(false);
+            Drawable originalIcon = ContextCompat.getDrawable(getApplicationContext(), R.drawable.greenwheel);
+            Drawable icon = convertDrawableToGrayScale(originalIcon);
+            myBtn.setImageDrawable(icon);
+        }
+    }
+
+    public void orangeSel(View v) {
+        switch(player){
+            case 1:
+                Globs.p1 = ORANGE;
+                break;
+            case 2:
+                Globs.p2 = ORANGE;
+                break;
+            case 3:
+                Globs.p3 = ORANGE;
+                break;
+            case 4:
+                Globs.p4 = ORANGE;
+                break;
+            case 5:
+                Globs.p5 = ORANGE;
+                break;
+            case 6:
+                Globs.p6 = ORANGE;
+                break;
+        }
+        if(player == 6 || player == Globs.playerCnt){
+            Intent intent = new Intent(this, LocalPlayGame.class);
+            startActivity(intent);
+        }
+        else {
+            player++;
+            TextView tv1 = (TextView) findViewById(R.id.textColorSel);
+            tv1.setText("Player " + player + " Select\n   Your Color");
+            ImageButton myBtn = (ImageButton)v.findViewById(R.id.imageButton7);
+            myBtn.setEnabled(false);
+            Drawable originalIcon = ContextCompat.getDrawable(getApplicationContext(), R.drawable.orangewheel);
+            Drawable icon = convertDrawableToGrayScale(originalIcon);
+            myBtn.setImageDrawable(icon);
+        }
     }
 }
